5e3cfc00983786bc9d9351a72198d0f758fc6711
Gabriel Ribeiro <55985861+Kaina-Ribeiro@users.noreply.github.com>
2022-05-01 12:22:38 -0300

    files upload

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
@@ -12,4 +17,7 @@ categoriesRoutes.get("/", (req, res) => {
   return listCategoriesController.handle(req, res);
 });
 
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  return importCategoryController.handle(req, res);
+});
 export { categoriesRoutes };
