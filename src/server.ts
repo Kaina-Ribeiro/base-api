d602cf41384bda671760f6d2e0078a8c78cbfbfb
Gabriel Ribeiro <55985861+Kaina-Ribeiro@users.noreply.github.com>
2022-05-01 20:18:11 -0300

    connect to postgres using typeORM

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
+import "./database";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -10,4 +11,4 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json());
 app.use(router);
 
-app.listen(3001);
+app.listen(3001, () => console.log("Server is running!"));
